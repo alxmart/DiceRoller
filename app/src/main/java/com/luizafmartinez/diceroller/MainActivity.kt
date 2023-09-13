107434f68f66c2ebb2b836e5d6d94748a946f4f4
alxmart <alxmartinez@gmail.com>
2023-09-13 18:49:12 -0300

    Layout modifications.(lesson) + Implementing ViewBinding.

--- app/src/main/java/com/luizafmartinez/diceroller/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/diceroller/MainActivity.kt
@@ -2,10 +2,32 @@ package com.luizafmartinez.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.luizafmartinez.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding) {
+
+           rollButton.text = "Let's Roll"
+
+            rollButton.setOnClickListener {
+                rollButton.text = "ROLL!!"
+                Toast.makeText(this@MainActivity,
+                    "HELLO",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
+
