1a8c756941b61ca6854eb5110bbf01678aab0e2d
alxmart <alxmartinez@gmail.com>
2023-09-14 17:24:43 -0300

    Added code to "roll the dice".

--- app/src/main/java/com/luizafmartinez/diceroller/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/diceroller/MainActivity.kt
@@ -20,24 +20,31 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
 
-           rollButton.text = "Let's Roll"
+            rollButton.text = "Let's Roll"
 
             rollButton.setOnClickListener {
-
                /*     Toast.makeText(this@MainActivity,
                     "HELLO",
                     Toast.LENGTH_SHORT
                 ).show()*/
-
                rollDice()
-
             }
         }
     }
 
-    fun rollDice() {
+    private fun rollDice() {
+
         val randomInt = Random().nextInt(6) + 1
-        binding.resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        binding.diceImage.setImageResource( drawableResource )
     }
 }
 
